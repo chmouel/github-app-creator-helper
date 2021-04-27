d105430d08ed237038ce0cc1547b5b208feb303c
Chmouel Boudjnah <chmouel@chmouel.com>
2021-04-27 12:30:15 +0200

    Wrap it up
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- pkg/kubestuff_test.go
+++ pkg/kubestuff_test.go
@@ -1,8 +1,15 @@
 package pkg
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
+
+	"github.com/google/go-github/v35/github"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestNew(t *testing.T) {
@@ -10,10 +17,35 @@ func TestNew(t *testing.T) {
 	if err != nil {
 		t.Fail()
 	}
-	route, err := c.GetFirstTLSRoute()
+
+	r, err := os.Open("/tmp/a.json")
 	if err != nil {
 		t.Fail()
 	}
-	fmt.Println(route.Spec.Host)
-	fmt.Println(route.Spec.TLS)
+	bits, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fail()
+	}
+	var manifest github.AppConfig
+	json.Unmarshal(bits, &manifest)
+
+	data := map[string][]byte{
+		"application_id": []byte(fmt.Sprintf("%d", manifest.GetID())),
+		"private.key":    []byte(manifest.GetPEM()),
+		"webhook.secret": []byte(manifest.GetWebhookSecret()),
+	}
+
+	var secretName = "github-app-secret"
+
+	if sec, _ := c.kubeClient.CoreV1().Secrets(c.Namespace).Get(secretName, metav1.GetOptions{}); sec != nil {
+		fmt.Printf("We already have a secret called %s\n", secretName)
+		t.Fail()
+	}
+	c.kubeClient.CoreV1().Secrets(c.Namespace).Create(&corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: secretName,
+		},
+		Data: data,
+	})
+
 }
--- pkg/web.go
+++ pkg/web.go
@@ -1,15 +1,21 @@
 package pkg
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"io"
 	"net/http"
 	"path/filepath"
 
+	"github.com/google/go-github/v35/github"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+const secretName = "github-app-secret"
 
 // TemplateRenderer is a custom html/template renderer for Echo framework
 type TemplateRenderer struct {
@@ -21,6 +27,8 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 }
 
 func index(c echo.Context) error {
+	var warning string
+
 	client, err := NewClient()
 	if err != nil {
 		return err
@@ -30,22 +38,39 @@ func index(c echo.Context) error {
 		return err
 	}
 
+	if sec, _ := client.kubeClient.CoreV1().Secrets(client.Namespace).Get(secretName, metav1.GetOptions{}); sec.Name != "" {
+		warning = fmt.Sprintf("We already have a secret called '%s' in namespace '%s', you need to delete it before you click on Submit or it would not work.\n", secretName, client.Namespace)
+	}
+
 	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
 		"webhook_route_host": route.Spec.Host,
+		"warning":            warning,
 	})
 }
 
 func redirect(c echo.Context) error {
 	code := c.QueryParam("code")
-	return c.String(http.StatusOK, "OK: "+code)
-	// ghclient := github.NewClient(nil)
-	// ctx := context.Background()
-	// ret, _, err := ghclient.Apps.CompleteAppManifest(ctx, code)
-	// if err != nil {
-	//	return err
-	// }
-	// ret.GetClientSecret()
-	// return c.String(http.StatusOK, "OK")
+	ghclient := github.NewClient(nil)
+	ctx := context.Background()
+	manifest, _, err := ghclient.Apps.CompleteAppManifest(ctx, code)
+	if err != nil {
+		return err
+	}
+	client, err := NewClient()
+	if err != nil {
+		return err
+	}
+	client.kubeClient.CoreV1().Secrets(client.Namespace).Create(&corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: secretName,
+		},
+		Data: map[string][]byte{
+			"application_id": []byte(fmt.Sprintf("%d", manifest.GetID())),
+			"private.key":    []byte(manifest.GetPEM()),
+			"webhook.secret": []byte(manifest.GetWebhookSecret()),
+		},
+	})
+	return c.Redirect(301, manifest.GetHTMLURL())
 }
 
 func Server() (err error) {
